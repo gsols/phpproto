57a79f16dfbaa4cf07c401337f87d9a6e3238cb6
nickaguilarh <nickaguilarh@users.noreply.github.com>
2023-12-08 08:29:39 +0000

    Merge pull request #6 from gsols/release-please--branches--main [gsols/proto-apis@57cf8c5618ef7576e58f7609d4f0dacdf21d8003]

---
+++ src/Queuer/Messages/v1/MessageServiceClient.php
@@ -0,0 +1,33 @@
+<?php
+// GENERATED CODE -- DO NOT EDIT!
+
+namespace GSols\PHPProto\Queuer\Messages\v1;
+
+/**
+ */
+class MessageServiceClient extends \Grpc\BaseStub {
+
+    /**
+     * @param string $hostname hostname
+     * @param array $opts channel options
+     * @param \Grpc\Channel $channel (optional) re-use channel object
+     */
+    public function __construct($hostname, $opts, $channel = null) {
+        parent::__construct($hostname, $opts, $channel);
+    }
+
+    /**
+     * @param \GSols\PHPProto\Queuer\Messages\v1\AcknowledgeRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     * @return \Grpc\UnaryCall
+     */
+    public function Acknowledge(\GSols\PHPProto\Queuer\Messages\v1\AcknowledgeRequest $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/queuer.messages.v1.MessageService/Acknowledge',
+        $argument,
+        ['\GSols\PHPProto\Queuer\Messages\v1\AcknowledgeResponse', 'decode'],
+        $metadata, $options);
+    }
+
+}
